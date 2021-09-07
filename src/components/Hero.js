d7c811c1c31f2d67f0ea1d2a7bfe7b5a38fe1eec
Albenoo <albeniskerqeli5@gmail.com>
2021-09-07 22:13:24 +0200

    Updating Table Component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,8 @@
-import Table from './Table';
 import React from 'react';
 import Row from './Row';
+
+const Table = React.lazy(() => import('../components/Table'));
+
 const Hero  = React.memo(() => {
 return(
     <section className="w-full   py-4 flex flex-col items-center justify-center flex-wrap">
