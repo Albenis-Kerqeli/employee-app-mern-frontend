02c29d3089d54e81c124a9e6901d79ee5298195d
Albenis Kërqeli <albeniskerqeli5@gmail.com>
2021-09-10 01:46:04 +0200

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import Row from './Row'
-import Table from './Table'
+const Row = React.lazy(() => import('./Row'));
+const Table = React.lazy(() => import('./Table'));
 const Hero  = () => {
 return(
     <section className="w-full   py-4 flex flex-col items-center justify-center flex-wrap">
